a5bb934a711994eeea0ba004315acc9357f2b22f
Alweare <yanis.dumont@gmail.com>
2020-11-24 15:01:38 +0100

    Début du CSS / sttructure page d'accueil faite

--- index.php
+++ index.php
@@ -5,100 +5,104 @@
 <!DOCTYPE html>
 
 <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="style.css" />
+        <title>GBAF</title>
+    </head>
 
-    <header>         <!-- En-tête de la page -->
-      
-        <head>
-                <meta charset="UTF-8">
-                <link rel="stylesheet" href="style.css" />
-                <title>GBAF</title>
-            </head>
-                <p><img src="images/logo_gbaf.png" alt="Logo GBAF" class="logo_gbaf"/></p> 
 
-                <?php
-                        //Récupérer le nom et prénom de la personne (et son image de profil ? )
-                ?>
+    
 
-    </header>
+    <body>
+        <!-- En-tête de la page -->
+        <header>   
+            <p>
+                <img src="images/logo_gbaf.png" alt="Logo GBAF" class="logo_gbaf"/>
+            </p> 
 
-        <body>
+            <?php
+                //Récupérer le nom et prénom de la personne (et son image de profil ? )
+            ?>
 
-            <section> <!-- section présentation -->
+        </header>
+        <!-- section présentation -->
+        <section id="presentation">  
+            
+            <h1>Banque humaine</h1> <!-- à modifier avec le vrai texte -->
+
+            <p>
+                <img src="" alt="Bannière gbaf" class="banniere" />
+            </p>
+
+
+        </section>
+        <section>
+
+            <h2>acteurs et partenaire</h2> <!-- à modifier avec le vrai texte -->
+
+            <article>
+                <img src="logoacteurs/CDE.png" alt="logo_CDE" class="Logo_CDE" />     <!-- changer le alt logo par logo_(nom de l'acteur )-->
+                <h3>
+                    <!-- sous titre a mettre -->
+                </h3>
+                <p>
+                    La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation.<br />
+                    Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.<br />
+                </p>
+            
                 
-                <h1>La GBAFF meilleur banque</h1> <!-- à modifier avec le vrai texte -->
+                <a href="acteur.php?id=<?php echo "ID"; ?>" id="bouton_lire_la_suite">Lire la suite</a>
+            </article>
 
-                <p><img src=""alt="Bannière gbaf" class="banniere" /></p>
+            <article>
+                <img src="logoacteurs/Dsa_france.png" alt="logo_Dsa_France" class="Logo_Dsa" />
+                <h3>
+                    <!-- sous titre a mettre -->
+                </h3>
+                <p>
+                    
+                    Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.<br />
+                    Nous accompagnons les entreprises dans les étapes clés de leur évolution.<br />
+                    Notre philosophie : s’adapter à chaque entreprise.<br />
+                    Nous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises<br />
 
+                </p>
+            
+                <a href="acteur.php?id=<?php echo "ID"; ?>" id="bouton_lire_la_suite">Lire la suite</a>
 
-            </section>
-            <section>
+            </article>
 
-                    <h2>acteurs et partenaire</h2> <!-- à modifier avec le vrai texte -->
+            <article>
+                <img src="logoacteurs/formation_co.png" alt="logo_formation_co" class="Logo_formation" />
+                <h3>
+                    <!-- sous titre a mettre -->
+                </h3>
+                <p>
+                    Formation&co est une association française présente sur tout le territoire.<br />
+                    Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.<br />
 
-                        <article>
-                            <img src="LogoActeurs/CDE.png" alt="logo_CDE" class="Logo_CDE" />     <!-- changer le alt logo par logo_(nom de l'acteur )-->
-                            <h3>
-                                <!-- sous titre a mettre -->
-                            </h3>
-                                <p>
-                                    La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation.<br />
-                                    Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.<br />
-                                </p>
-                        
-                            
-                            <a href="" id="bouton_lire_la_suite">Lire la suite</a>
-                        </article>
+                </p>
 
-                        <article>
-                            <img src="LogoActeurs/Dsa_france.png" alt="logo_Dsa_France" class="Logo_Dsa" />
-                            <h3>
-                                <!-- sous titre a mettre -->
-                            </h3>
-                                   <p>
-                                       
-                                    Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.<br />
-                                    Nous accompagnons les entreprises dans les étapes clés de leur évolution.<br />
-                                    Notre philosophie : s’adapter à chaque entreprise.<br />
-                                    Nous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises<br />
+                <a href="acteur.php?id=<?php echo "ID"; ?>" id="bouton_lire_la_suite">Lire la suite</a>
 
-                                   </p>
-                            
-                            <a href="" id="bouton_lire_la_suite">Lire la suite</a>
-
-                        </article>
-
-                        <article>
-                            <img src="LogoActeurs/formation_co.png" alt="logo_formation_co" class="Logo_formation" />
-                            <h3>
-                                <!-- sous titre a mettre -->
-                            </h3>
-                            <p>
-                                Formation&co est une association française présente sur tout le territoire.<br />
-                                Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.<br />
-
-                            </p>
-
-                            <a href="" id="bouton_lire_la_suite">Lire la suite</a>
-
-                        </article>
-            </section>
+            </article>
+        </section>
 
 
-        </body>
+    </body>
 
-        <footer>
-            <ul>
-                <li>
-                    <a href="">Mentions légales</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
-
-
-
-            </ul>
+    <footer>
+        <ul>
+            <li>
+                <a href="">Mentions légales</a>
+            </li>
+            <li>
+                <a href="">Contact</a>
+            </li>
+        </ul>
             
             
-        </footer>
+    </footer>
 </html>
